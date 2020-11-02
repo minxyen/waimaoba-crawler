1cc648593d995016f02cf910c4e38b3b4a42993d
Minyen Hsieh <stevehsieh2797@gmail.com>
2020-11-02 15:37:15 +0800

    refactored, wrote retrieve data into Excel

--- waimaoba_crawler/main.py
+++ waimaoba_crawler/main.py
@@ -1,5 +1,24 @@
+from waimaoba_crawler.get_company_info import get_urls
 from waimaoba_crawler.get_company_info import get_company_info
 
+import pandas as pd
 
-if __name__ == '__main__':
-    get_company_info()
+
+start_page = input('Start From: ')
+end_page = input('End at: ')
+
+urls = get_urls(start_page, end_page)
+print(len(urls))
+
+data_list = []
+for i in range(len(urls)):
+    company_info = get_company_info(urls[i])
+    data_list.append(company_info)
+
+print(data_list)
+
+df = pd.DataFrame(data_list)
+print(df)
+df.to_excel(f'waimaoba{start_page}-{end_page}.xlsx', index=False)
+
+
